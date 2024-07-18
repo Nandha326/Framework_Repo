dc0c4f3525ce8042d14b5b9cdb4973d27d631837
Nandha GitHub <Nandhakumar.d@nandha.net>
2024-07-18 15:56:16 +0530

    0

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/listenerUtility/ListImpClass.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/listenerUtility/ListImpClass.java
@@ -4,21 +4,11 @@ package com.comcast.crm.listenerUtility;
  */
 
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
-
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ISuite;
 import org.testng.ISuiteListener;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-import org.testng.annotations.Test;
-
-import com.comcast.crm.basetest.BaseClass;
 
 public class ListImpClass implements ITestListener,ISuiteListener{
 	
@@ -44,17 +34,9 @@ public class ListImpClass implements ITestListener,ISuiteListener{
 	
 		public void onTestFailure(ITestResult result) {
 		// TODO Auto-generated method stub
-			String testName = result.getMethod().getMethodName();
-			EventFiringWebDriver edriver = new EventFiringWebDriver(BaseClass.sdriver);
-			File srcFile = edriver.getScreenshotAs(OutputType.FILE);
-			String time =new Date().toString().replace(" ","_").replace(":","_");
-			try {
-				FileUtils.copyFile(srcFile, new File("./screenshot/"+testName+"+"+time+".png"));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			
 			}
-		}
+		
 		
 	
 		public void onTestSkipped(ITestResult result) {
